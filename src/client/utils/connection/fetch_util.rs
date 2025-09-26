9ccb13178fd890dd6386338b7fbf56fc0cf055cd
wingedcap <wingedcap@proton.me>
2025-09-26 12:50:31 +0200

    fetch util use wss scheme on web if loaded over https

--- src/client/utils/connection/fetch_util.rs
+++ src/client/utils/connection/fetch_util.rs
@@ -23,7 +23,20 @@ where
 {
     let timeout_ms = 10000;
 
-    let url = format!("ws://{}", server.host);
+    #[cfg(not(target_family = "wasm"))]
+    let scheme = "ws";
+
+    #[cfg(target_family = "wasm")]
+    let scheme = {
+        let window = web_sys::window().ok_or("Failed to get window location href")?;
+        let location = window.location();
+        let href = location.href().map_err(|_| "Failed to get href")?;
+        let href_scheme = href.split("://").nth(0).ok_or("Failed to get scheme")?;
+        let is_secure = href_scheme == "https";
+        if is_secure { "wss" } else { "ws" }
+    };
+
+    let url = format!("{}://{}", scheme, server.host);
 
     // Connect to the WebSocket server
     let socket = connect(url).await.map_err(|e| {
